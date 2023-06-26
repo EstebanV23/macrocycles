bc14a2dc55a272a329c8a7b5b3fc75a4aeda0a94
estebandev <brayan.villa.fer@gmail.com>
2023-06-25 23:09:12 -0500

    fix id 0

--- macrocycles/src/services/serviceAddMacroToUser.js
+++ macrocycles/src/services/serviceAddMacroToUser.js
@@ -4,11 +4,8 @@ import serviceUpdateUser from './serviceUpdateUser'
 
 export default async function serviceAddMacroToUser (macro, user) {
   const allMacrosUser = await serviceGetAllMacrosUser(user)
-  console.log('🚀 ~ file: serviceAddMacroToUser.js:7 ~ serviceAddMacroToUser ~ allMacrosUser:', allMacrosUser)
   const macrosToArr = allMacrosUser.data.macrocycles ? transformToArrayId(allMacrosUser.data.macrocycles) : []
-  console.log('🚀 ~ file: serviceAddMacroToUser.js:9 ~ serviceAddMacroToUser ~ macrosToArr:', macrosToArr)
   const newMacrosToUser = macrosToArr.length === 0 ? [{ id: macro.id }] : [...macrosToArr, { id: macro.id }]
-  console.log('🚀 ~ file: serviceAddMacroToUser.js:11 ~ serviceAddMacroToUser ~ newMacrosToUser:', newMacrosToUser)
   const updateUser = await serviceUpdateUser(user, { macrocycles: newMacrosToUser })
   return updateUser
 }
--- macrocycles/src/services/serviceNewMicrocycle.js
+++ macrocycles/src/services/serviceNewMicrocycle.js
@@ -2,7 +2,7 @@ import { API_URL } from '../config/requests'
 import addSnakeCaseDates from '../logic/addSnakeCaseDates'
 
 export default async function serviceNewMicrocycle (microcycle) {
-  if (microcycle.id) delete microcycle.id
+  if (microcycle.id || microcycle.id === 0 || microcycle.id === '') delete microcycle.id
   const newMicro = microcycle // addSnakeCaseDates(microcycle)
   return fetch(`${API_URL}/microcycle`, {
     method: 'POST',
